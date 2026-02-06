92dc03868964a0e1810202c44df0f855c129f7f3
dedlocc <dedlocc@users.noreply.github.com>
2026-02-06 00:26:27 +0000

    deploy: 61549a45bd1368264b675a7adb2167b04acd7de1

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-6e1b535f.js',
+                path_to_root + 'searchindex-ba293cdc.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
